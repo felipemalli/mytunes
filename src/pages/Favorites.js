2ecc65d47b92126fd6b3a2f322f7b336c32fc235
FelipeVahia <felipevahia@gmail.com>
2021-11-11 15:38:43 -0300

    1) Realized

---
+++ src/pages/Favorites.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div data-testid="page-favorites">aaaa</div>
+    );
+  }
+}
+
+export default Favorites;
---
+++ src/pages/Search.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div data-testid="page-search">aaaa</div>
+    );
+  }
+}
+
+export default Search;
