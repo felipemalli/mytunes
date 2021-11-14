7675d1468ad2586cb2e5d2295bf76d2ef685a1f1
FelipeVahia <felipevahia@gmail.com>
2021-11-14 04:33:59 -0300

    6) Realized

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
@@ -11,6 +12,7 @@ class Search extends Component {
       artistName: '',
       isLoading: false,
       albumList: [],
+      artistNameSaved: '',
     };
   }
 
@@ -21,7 +23,19 @@ class Search extends Component {
 
   onClickButton = () => {
     const { artistName } = this.state;
-    this.setState({ isLoading: true, albumList: searchAlbumsAPI((artistName)) });
+    this.setState({ isLoading: true },
+      () => searchAlbumsAPI((artistName))
+        .then((info) => this.setState(
+          {
+            albumList: info,
+            isLoading: false,
+            artistNameSaved: artistName,
+          },
+        )));
+  }
+
+  isButtonClicked = () => {
+
   }
 
   render() {
@@ -29,37 +43,55 @@ class Search extends Component {
       artistName,
       isLoading,
       albumList,
+      artistNameSaved,
     } = this.state;
 
     const REQUIRED_CHARACTERS = 2;
     const canLogin = artistName.length < REQUIRED_CHARACTERS;
+    const artistAndNoAlbum = (artistName && albumList.length === 0);
 
     return (
       <div data-testid="page-search">
         <Header />
-        <input
-          name="artistName"
-          placeholder="Nome do Artista"
-          type="text"
-          data-testid="search-artist-input"
-          onChange={ this.onInputChange }
-        />
-
-        <button
-          type="submit"
-          disabled={ canLogin }
-          data-testid="search-artist-button"
-          onClick={ this.onClickButton }
-        >
-          Pesquisar
-        </button>
-        <p>
-          Resultado de álbuns de:
-          {' '}
-          {artistName}
-        </p>
-        <p>{albumList}</p>
-        {isLoading && <Loading />}
+        <div>
+          {isLoading ? <Loading /> : (
+            <div>
+              <input
+                name="artistName"
+                placeholder="Nome do Artista"
+                type="text"
+                data-testid="search-artist-input"
+                onChange={ this.onInputChange }
+              />
+              <button
+                type="submit"
+                disabled={ canLogin }
+                data-testid="search-artist-button"
+                onClick={ this.onClickButton }
+              >
+                Pesquisar
+              </button>
+            </div>
+          )}
+          <div>
+            <p>
+              {albumList.length !== 0 && `Resultado de álbuns de: ${artistNameSaved}`}
+              {artistAndNoAlbum && 'Nenhum álbum foi encontrado'}
+            </p>
+            <ul>
+              {albumList.map((album) => (
+                <li key={ album.collectionId }>
+                  <Link
+                    to={ `/album/${album.collectionId}` }
+                    data-testid={ `link-to-album-${album.collectionId}` }
+                  >
+                    <span>{album.collectionName}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
