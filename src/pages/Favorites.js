8d1bee1646354f9fcf6bf488ee96e1cc58e2c32e
FelipeVahia <felipevahia@gmail.com>
2021-11-16 16:34:24 -0300

    11) Realized

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      favSongs: [],
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.updateFavSongs();
+  }
+
+  removeSong = ({ target }) => {
+    const { favSongs } = this.state;
+    this.setState({ isLoading: true });
+    if (!target.checked) {
+      removeSong(favSongs.find((music) => music.trackId === Number(target.id)))
+        .then(() => this.updateFavSongs());
+    }
+  }
+
+  updateFavSongs = () => {
+    this.setState({ isLoading: true });
+    getFavoriteSongs().then((info) => this.setState({
+      favSongs: info.filter((eachInfo) => eachInfo.trackId !== undefined),
+      isLoading: false,
+    }));
+  }
+
   render() {
+    const { isLoading, favSongs } = this.state;
+
     return (
-      <div data-testid="page-favorites">
+      <div>
         <Header />
+        {isLoading ? <Loading /> : (
+          <div>
+            <ul>
+              {favSongs.map((music) => (<MusicCard
+                key={ music.trackId }
+                trackName={ music.trackName }
+                previewUrl={ music.previewUrl }
+                trackId={ music.trackId }
+                onChange={ this.removeSong }
+                isChecked
+              />))}
+            </ul>
+          </div>
+        )}
       </div>
     );
   }
